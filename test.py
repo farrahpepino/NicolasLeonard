d14168c5cee54e78e3708aab56b7680606baa90c
farrah <idcfarrah@gmail.com>
2025-07-13 20:37:34 +0000

    Added intro file

---
+++ test.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+print("Hello my name is Nicolas")
+print("I'm an iPad kid")
+print("My favorite colors are blue and pink")
+print("My favorite food is olives")
+print("I have a cat named Bella")
